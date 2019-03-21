96fdcb9b2fa7496f8ac03b3c14f0825ca68127df
Ivan I. Ovchinnikov <mail@iovchinnikov.ru>
2019-03-21 14:07:08 +0300

    Описание варианта решения с идентификатором

--- modules/global/src/ru/iovchinnikov/libfieldsample/entity/UidValSettings.java
+++ modules/global/src/ru/iovchinnikov/libfieldsample/entity/UidValSettings.java
@@ -18,7 +18,7 @@ public class UidValSettings extends StandardEntity {
     private static final long serialVersionUID = 8085328611300322081L;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "ENTITY_ID")
+    @JoinColumn(name = "ENTITY_ID", unique = true)
     protected ValueHolder entity;
 
     @JoinTable(name = "LIBFIELDSAMPLE_ID_VAL_SETTINGS_LIB_ENTITY_LINK",
